6efbfb4f7cfd10bddfc0eccc3eaf07a5a1f60a6e
gabrielfu <hfu.gabriel@gmail.com>
2020-08-25 19:29:02 +0800

    ensure only max 8 char per plate

--- src/models/lpr.py
+++ src/models/lpr.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 
 class LPR():
     def __init__(self, cfg, use_trt={}):
@@ -124,6 +125,13 @@ class LPR():
             chars = []
             # Predict plate num from CharRecognizer
             if preds is not None:
+                # HK license plates have max 8 characters,
+                # so select top 8 highest score (conf*cls_conf) characters if too many
+                if len(preds) > 8:
+                    scores = preds[:,4]
+                    indices = np.argsort(scores)[::-1][:8].astype(np.uint8) # get top 8 indices
+                    preds = preds[indices, ...]
+                # Feed into char recognizer
                 for box in preds:
                     x1,y1,x2,y2 = box[:4]
                     chars.append(batch_plates_imgs[i][y1:y2, x1:x2])
