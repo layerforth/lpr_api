e48ba415260e2515820e21b55926f092f15bf91a
Mike Wong <wongmike1122@gmail.com>
2020-08-25 19:00:51 +0800

    changed structure

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from src.models.lpr import LPR
+from src.models.char_recognizer import CharRecognizer
+from src.models.plate_detector import PlateDetector
+from src.models.segmentator import Segmentator
+from src.models.plate_detector_trt import PlateDetectorTRT
+from src.models.segmentator_trt import SegmentatorTRT
